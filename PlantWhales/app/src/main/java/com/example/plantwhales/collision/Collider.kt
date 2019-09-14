e1e7e622746faf4c7d21ca2db47ec1bd32591587
Str4thus <trashytrashmail42@gmail.com>
2019-09-14 11:22:16 +0200

    Update Collider.kt
    closes #4 and #5

--- PlantWhales/app/src/main/java/com/example/plantwhales/collision/Collider.kt
+++ PlantWhales/app/src/main/java/com/example/plantwhales/collision/Collider.kt
@@ -2,7 +2,6 @@ package com.example.plantwhales.collision
 
 import com.example.plantwhales.gamelogic.Physics
 import com.example.plantwhales.gameobjects.GameObject
-import com.example.plantwhales.maths.Vector2
 
 abstract class Collider(var gameObject: GameObject) {
     fun overlaps(other: Collider): Boolean {
